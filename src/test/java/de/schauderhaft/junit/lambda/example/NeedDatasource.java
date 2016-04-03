fb767af256c3ed0b21064d1e9233b2784f282f48
Jens Schauder <jens.schauder@freenet.de>
2016-04-03 19:04:25 +0200

    Slides for demonstrating bundling of before and after

--- src/test/java/de/schauderhaft/junit/lambda/example/NeedDatasource.java
+++ src/test/java/de/schauderhaft/junit/lambda/example/NeedDatasource.java
@@ -2,5 +2,4 @@ package de.schauderhaft.junit.lambda.example;
 
 public interface NeedDatasource {
     void set(MyDatasource ds);
-
 }
--- src/test/java/de/schauderhaft/junit/lambda/example/WithDatasource.java
+++ src/test/java/de/schauderhaft/junit/lambda/example/WithDatasource.java
@@ -7,13 +7,13 @@ import org.junit.gen5.api.extension.TestExtensionContext;
 public class WithDatasource implements BeforeEachExtensionPoint, AfterEachExtensionPoint {
 
     @Override
-    public void beforeEach(TestExtensionContext testExtensionContext) throws Exception {
+    public void beforeEach(TestExtensionContext testExtensionContext) {
         System.out.println("before");
         ((NeedDatasource)testExtensionContext.getTestInstance()).set(new MyDatasource());
     }
 
     @Override
-    public void afterEach(TestExtensionContext testExtensionContext) throws Exception {
+    public void afterEach(TestExtensionContext testExtensionContext) {
         System.out.println("after");
         ((NeedDatasource)testExtensionContext.getTestInstance()).set(null);
     }
